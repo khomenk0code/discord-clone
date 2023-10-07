b3d5c9c44f89cc43b26b2b06fbaa0c39f582e06b
Anton Khomenko <regiysa@gmail.com>
2023-10-07 16:33:13 +0300

    chore: setup dark\light mode, custom colors

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from '@clerk/nextjs'
+import { ModeToggle } from '@/components/theme-toggle'
 
 export default function Home() {
     return (
         <div>
             <UserButton afterSignOutUrl="/" />
+            <ModeToggle />
         </div>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                soft: '#a4a4a4',
+                main: '#0EA5E9FF',
+                mainAlt: '#0d43dc',
+                light: '#a2a1a6',
+                dark: '#313338',
+                darker: '#1d1d23',
+                white: '#FFF',
+                error: '#ff002d',
+                transparent: 'transparent',
+                current: 'currentColor',
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 export default config
