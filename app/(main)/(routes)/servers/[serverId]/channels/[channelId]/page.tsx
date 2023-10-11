1004eb2ac1c49db1cc9aeca9b008cf292db86652
Anton Khomenko <regiysa@gmail.com>
2023-10-11 17:10:16 +0300

    chore: chat header, mobile toggle menu

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,11 +1,48 @@
 import { NextPage } from 'next'
+import { currentProfile } from '@/lib/current-profile'
+import { redirectToSignIn } from '@clerk/nextjs'
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
+import ChatHeader from '@/components/chat/chat-header'
 
-interface ChannelIdProps {}
+interface ChannelIdProps {
+    params: {
+        serverId: string
+        channelId: string
+    }
+}
 
-const ChannelId: NextPage<ChannelIdProps> = () => {
+const ChannelId: NextPage<ChannelIdProps> = async ({ params }) => {
+    const profile = await currentProfile()
+
+    if (!profile) {
+        return redirectToSignIn()
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        },
+    })
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile.id,
+        },
+    })
+
+    if (!channel || !member) {
+        redirect('/')
+    }
+
     return (
-        <div>
-            <div>ChannelId page</div>
+        <div className="bg-white dark:bg-dark flex flex-col h-full">
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type={'channel'}
+            />
         </div>
     )
 }
---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,30 @@
+import { NextPage } from 'next'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button'
+import { Menu } from 'lucide-react'
+import NavigationSidebar from '@/components/navigation/navigation-sidebar'
+import ServerSidebar from '@/components/server/server-sidebar'
+
+interface MobileToggleProps {
+    serverId: string
+}
+
+const MobileToggle: NextPage<MobileToggleProps> = ({ serverId }) => {
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0 flex gap-0">
+                <div className="w-[72px]">
+                    <NavigationSidebar />
+                </div>
+                <ServerSidebar serverId={serverId} />
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+export default MobileToggle
